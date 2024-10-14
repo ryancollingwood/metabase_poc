edd76447f60805bcdfbe46a0539768e12b58f2c1
Ryan Collingwood <ryancollingwood@gmail.com>
2024-10-15 08:05:23 +1100

    Update update_baserow.py

--- update_baserow.py
+++ update_baserow.py
@@ -1,137 +1,194 @@
 from copy import deepcopy
 import os
+from time import sleep
 from baserowapi import Baserow, Filter, GenericField, Table
+from baserowapi import MultipleSelectField, SingleSelectField, DateField
 from typing import Any, Dict, List, Union
+from urllib3.exceptions import HTTPError
 
+class BaserowUpdater:
+    def __init__(self, baserow_url: str, baserow_api_key: str, table: Union[int, Table], schema: Dict[str, Any] = None, retry_max_count: int = 3, retry_wait_seconds: int = 10):
+        self.baserow_url = baserow_url.rstrip('/')
+        self.retry_max_count = retry_max_count
+        self.retry_wait_seconds = retry_wait_seconds
 
+        self.baserow = self._baserow_api(baserow_api_key)
 
-def baserow_api() -> Baserow:
-    baserow_url = os.getenv('BASEROW_URL')
-    baserow_api_key = os.getenv('BASEROW_API_KEY')
-
-    if baserow_url is None:
-        raise ValueError("BASEROW_URL is None")
-    if baserow_url[-1] == "/":
-        baserow_url = baserow_url[:-1]
-    
-    if baserow_api_key is None:
-        raise ValueError("BASEROW_API_KEY is None")
-
-    return Baserow(url=baserow_url, token=baserow_api_key)
-
-
-def baserow_table(table_id: int) -> Table:
-    baserow = baserow_api()
-    table = baserow.get_table(table_id)
-
-    return table
-
-
-def baserow_table_schema(table: Table) -> Dict[Dict, GenericField]:
-    table_fields = table.field_names
-
-    return {col: table.fields[col] for col in table_fields}
-
-
-def baserow_update_row(table: Union[int, Table], data: Dict[str, Any], schema: Dict[Dict, GenericField] = None) -> int:
-    """
-    Update a row in a Baserow table.
-
-    - For columns that aren't in the schema it will
-        - Check if the column is an option for a multiple select or single select column
-        - If it is, it will add the option to the column and remove the option from the data
-        - If it isn't, it will raise an error
-    - Checks for existence of a row by primary column value
-    - If row does not exist, adds a new row, if it does exist, updates the row
-
-    TODO:
-    - Add support for updating multiple rows at once
-    - Datetime support, given the various datetime formats in Baserow
-    - Numeric support, given the various numeric formats in Baserow
-    - File support?
-    """
-    if isinstance(table, int):
-        table = baserow_table(table)
-
-    if schema is None:
-        table = baserow_table(table)
-        schema = baserow_table_schema(table)
-    
-    primary_cols = [x for x in schema.values() if x.is_primary]
-    if len(primary_cols) == 0:
-        raise ValueError("No primary column found")
-    
-    filters = [Filter(x.name, data[x.name]) for x in primary_cols]
-    matching_rows = table.get_rows(filters=filters, filter_type='AND')
-
-    row_id = -1
-    if len(matching_rows) == 1:
-        row_id = matching_rows[0].id
-    elif len(matching_rows) > 1:
-        raise ValueError("Multiple rows found")
-    
-    for col in primary_cols:
-        if col.is_read_only:
-            raise ValueError("Primary column is read only")
-        if col.name not in data:
-            raise ValueError("Primary column is missing")
-    
-    update_data = deepcopy(data)
-    option_cols = [x for x in schema.values() if x.TYPE in ["single_select", "multiple_select"]]
-    option_values = dict()
-
-    for col in option_cols:
-        col_name = col.name
-        option_values[col_name] = list()
-        for option in col.options:
-            if option in update_data:
-                if update_data[option] in [1, True]:
-                    option_values[col_name].append(option)
-                update_data.pop(option, None)
-
-        if col.TYPE == "single_select":
-            if len(option_values[col_name]) > 1:
-                raise ValueError(f"Multiple options for single select: {option_values[col_name]}")
-    
-    for col in option_cols:
-        col_name = col.name
-        if col.TYPE == "single_select":
-            update_data[col_name] = option_values[col_name][0]
+        if isinstance(table, int):
+            self.table_id = table
+            self.__get_table()
+        elif isinstance(table, Table):
+            self.table = table
+            self.table_id = table.id
         else:
-            update_data[col_name] = option_values[col_name]
+            raise ValueError("Table must be int or Table")
 
-    for column_name in update_data.keys():
-        if column_name not in schema.keys():
-            raise ValueError(f"Column not found in schema: {column_name}")
-        
-    if row_id == -1:
-        added_row = table.add_rows(update_data)
-        row_id = added_row[0].id
-    else:
-        update_data["id"] = row_id
-        table.update_rows([update_data])
+        self.schema = schema
+        if schema is None:
+            self.__get_table_schema()
+
+
+    def _baserow_api(self, baserow_api_key: str) -> Baserow:
+        if not self.baserow_url:
+            raise ValueError("BASEROW_URL is None")
+
+        if not baserow_api_key:
+            raise ValueError("BASEROW_API_KEY is None")
+
+        return Baserow(url=self.baserow_url, token=baserow_api_key)
+
+    def __get_table(self) -> None:
+        self.table = self.baserow.get_table(self.table_id)
+
+    def __get_table_schema(self) -> None:
+        table_fields = self.table.field_names
+        self.schema = {col: self.table.fields[col] for col in table_fields}
+
+    def __upsert_row_to_table(self, update_data: Dict[str, Any], row_id: int, retry_count: int = 0) -> int:
+        try:
+            if row_id == -1:
+                added_row = self.table.add_rows(update_data)
+                return added_row[0].id
+            else:
+                update_data["id"] = row_id
+                self.table.update_rows([update_data])
+                return row_id
+        except HTTPError as e:
+            print("update_or_add_row - HTTPError", e)
+            if retry_count < self.retry_max_count:
+                sleep((retry_count + 1) * self.retry_wait_seconds)
+                return self.__upsert_row_to_table(update_data, row_id, retry_count + 1)
+            raise e
+        except Exception as e:
+            raise e
+
+    def find_rows(self, filters: List[Filter], filter_type='AND', retry_count=0):
+        try:
+            matching_rows = self.table.get_rows(filters=filters, filter_type=filter_type)
+        except HTTPError as e:
+            print("find_rows - HTTPError", e)
+            if retry_count < self.retry_max_count:
+                sleep((retry_count + 1) * self.retry_wait_seconds)
+                return self.find_rows(filters, filter_type, retry_count + 1)
+            raise e
+        except Exception as e:
+            raise e
+
+        return matching_rows
     
-    return row_id
+    def update_row(self, data: Dict[str, Any], schema: Dict[Dict, GenericField] = None) -> int:
+        schema = self.schema if schema is None else schema
+        primary_cols = [x for x in schema.values() if x.is_primary]
+        if len(primary_cols) == 0:
+            raise ValueError("No primary column found")
+
+        filters = [Filter(x.name, data[x.name]) for x in primary_cols]
+        matching_rows = self.find_rows(filters)
+
+        row_id = -1
+        if len(matching_rows) == 1:
+            row_id = matching_rows[0].id
+        elif len(matching_rows) > 1:
+            raise ValueError("Multiple rows found")
+
+        for col in primary_cols:
+            if col.is_read_only:
+                raise ValueError("Primary column is read only")
+            if col.name not in data:
+                raise ValueError("Primary column is missing")
+
+        update_data = deepcopy(data)
+
+        option_cols: List[Union[MultipleSelectField, SingleSelectField]] = [x for x in schema.values() if x.TYPE in ["single_select", "multiple_select"]]
+        option_values: Dict[str, List] = dict()
+
+        for col in option_cols:
+            col_name = col.name
+            option_values[col_name] = list()
+            for option in col.options:
+                if option in update_data:
+                    if update_data[option] in [1, True]:
+                        option_values[col_name].append(option)
+                    update_data.pop(option, None)
+
+            if col.TYPE == "single_select":
+                if len(option_values[col_name]) > 1:
+                    raise ValueError(f"Multiple options for single select: {option_values[col_name]}")
+
+        for col in option_cols:
+            col_option_values = option_values[col.name]
+
+            if len(col_option_values) == 0:
+                continue
+
+            if update_data.get(col.name) is not None:
+                continue
+            col_name = col.name
+            if col.TYPE == "single_select":
+                update_data[col_name] = col_option_values[0]
+            else:
+                update_data[col_name] = col_option_values
+
+        date_cols: List[DateField] = [x for x in schema.values() if x.TYPE == "date"]
+        for col in date_cols:
+            if col.name not in update_data:
+                continue
+
+            if not isinstance(update_data[col.name], datetime):
+                raise ValueError(f"Invalid date value for column: {col.name}")
+            
+            datetime_value: datetime = update_data[col.name]
+
+            if col.date_include_time:
+                if col.date_format == 'ISO':
+                    update_data[col.name] = datetime_value.isoformat()
+                elif col.date_format == 'US':
+                    update_data[col.name] = datetime_value.strftime('%m/%d/%Y %I:%M:%S %p')
+                else:
+                    # likely to fail if the date format is not supported
+                    update_data[col.name] = datetime_value.strftime(col.date_format)
+            else:
+                if col.date_format == 'ISO':
+                    update_data[col.name] = datetime_value.date().isoformat()
+                elif col.date_format == 'US':
+                    update_data[col.name] = datetime_value.date().strftime('%m/%d/%Y')
+                else:
+                    # likely to fail if the date format is not supported
+                    update_data[col.name] = str(datetime_value.date())
+
+        for column_name in update_data.keys():
+            if column_name not in schema.keys():
+                raise ValueError(f"Column not found in schema: {column_name}")
+
+        print(update_data)
+        row_id = self.__upsert_row_to_table(update_data, row_id)
+
+        return row_id
 
 if __name__ == "__main__":
     from dotenv import load_dotenv
+    from datetime import datetime
     load_dotenv()
 
-    table_id = 841
+    table_id = 842
 
-    table = baserow_table(table_id)
-    schema = baserow_table_schema(table)
+    table_updater = BaserowUpdater(os.getenv('BASEROW_URL'), os.getenv('BASEROW_API_KEY'), table_id)
 
-    print(schema)
+    print(table_updater.schema)
 
-    # this should fail because we've included a column that is not in the schema
-    data = {
-        "foo": 1,
-        "bar": 1,
-        "question": "What is your name?",
-        "extra": "Extra",
+    data ={
+        'Name': 'Hello world', 
+        'Notes': 'This is a test note', 
+        'Number': 123,
+        'Price': 123.45,
+        'Boolean': True,
+        'Date european': datetime(2024, 1, 1, 0, 0, 0, 0),
+        #'Date us include time': datetime(2024, 10, 12, 2, 35, 16, 552),
+        'Single select': 'Option 2',
+        'Multiple select': ['Option B', 'Option C'],
+        'Rating': 3,
     }
 
-    baserow_update_row(table, data, schema)
+    table_updater.update_row(data)
 
     print("Done")
